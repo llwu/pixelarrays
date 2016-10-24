76b2365b7739629c4b6e43d15bd0b5fdbc746c28
Lawrence Wu <llwu@llwu.me>
2016-10-23 21:50:43 -0400

    Add some performance results

--- tests/test_2d.py
+++ tests/test_2d.py
@@ -1,15 +1,13 @@
 """Tests plotting the solutions to 2D equations."""
 
+from time import clock
+
 import matplotlib.pyplot as plt
 
 from pixelarrays.util.plotting import plot, coarsen
 from pixelarrays.recursive import solve
 
-
-def paper_example1():
-    """Tests example from https://arxiv.org/pdf/1609.00061v1.pdf."""
-    f = plot(lambda x, w: x * x - w, -2.0, 2.0, -2.0, 2.0)
-    fs = coarsen(f)
+def show_plot(f):
     plt.imshow(f.T,
                cmap='gray',
                origin='lower',
@@ -17,21 +15,36 @@ def paper_example1():
                extent=[-2, 2, -2, 2])
     plt.show()
 
+
+def paper_example1():
+    """Tests example from https://arxiv.org/pdf/1609.00061v1.pdf."""
+    start = clock()
+    f = plot(lambda x, w: x * x - w, -2.0, 2.0, -2.0, 2.0)
+    end = clock()
+    print(end - start)
+    show_plot(f)
+
+    start = clock()
     g = plot(lambda w, y: w + y * y - 1, -2.0, 2.0, -2.0, 2.0)
+    end = clock()
+    print(end - start)
+    show_plot(g)
+
+    start = clock()
+    fs = coarsen(f)
+    end = clock()
+    print(end - start)
+
+    start = clock()
     gs = coarsen(g)
-    plt.imshow(gs[5].T,
-               cmap='gray',
-               origin='lower',
-               interpolation='none',
-               extent=[-2, 2, -2, 2])
-    plt.show()
+    end = clock()
+    print(end - start)
+
+    start = clock()
     s = solve(fs, gs)
-    plt.imshow(s.T,
-               cmap='gray',
-               origin='lower',
-               interpolation='none',
-               extent=[-2, 2, -2, 2])
-    plt.show()
+    end = clock()
+    print(end - start)
+    show_plot(s)
 
 if __name__ == '__main__':
     paper_example1()
